a2ce344e5ed2459c91ef54dcacbe94010e510e12
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-03-23 23:11:12 -0300

    :pencil2: refactor: Rename all `QueryBuilder`receivers

--- cimenteiro/builder.go
+++ cimenteiro/builder.go
@@ -13,65 +13,65 @@ type QueryBuilder struct {
 	organizers []elements.Expression
 }
 
-func (builder *QueryBuilder) Fields(fields ...string) *QueryBuilder {
-	index := len(builder.fields)
+func (query *QueryBuilder) Fields(fields ...string) *QueryBuilder {
+	index := len(query.fields)
 	// Extends the slice cap, to prevent reallocate inside the loop
 	newCap := len(fields) + index
-	builder.fields = builder.fields[:newCap]
+	query.fields = query.fields[:newCap]
 	for _, field := range fields {
-		builder.fields[index] = elements.FieldExpression{Name: field, Alias: ""}
+		query.fields[index] = elements.FieldExpression{Name: field, Alias: ""}
 		index++
 	}
-	return builder
+	return query
 }
 
 // FIXME: Add parameter for first tableName (to not use builder.tableName)
-func (builder *QueryBuilder) buildJoin(
+func (query *QueryBuilder) buildJoin(
 	tableName elements.TableName, firstColumn string, secondColumn string,
 ) elements.JoinClause {
 	return elements.JoinClause{
 		JoinType: elements.JoinAll,
 		Table:    tableName,
 		On: elements.Clause{
-			FirstHalf:  elements.FieldExpression{Name: builder.tableName.Column(firstColumn)},
+			FirstHalf:  elements.FieldExpression{Name: query.tableName.Column(firstColumn)},
 			Operator:   elements.OperatorEqual,
 			SecondHalf: elements.FieldExpression{Name: tableName.Column(secondColumn)},
 		},
 	}
 }
 
-func (builder *QueryBuilder) LeftJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
-	leftJoin := builder.buildJoin(tableName, firstColumn, secondColumn)
+func (query *QueryBuilder) LeftJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
+	leftJoin := query.buildJoin(tableName, firstColumn, secondColumn)
 	leftJoin.JoinType = elements.JoinLeft
-	builder.joins = append(builder.joins, leftJoin)
-	return builder
+	query.joins = append(query.joins, leftJoin)
+	return query
 }
 
-func (builder *QueryBuilder) RightJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
-	rightJoin := builder.buildJoin(tableName, firstColumn, secondColumn)
+func (query *QueryBuilder) RightJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
+	rightJoin := query.buildJoin(tableName, firstColumn, secondColumn)
 	rightJoin.JoinType = elements.JoinRight
-	builder.joins = append(builder.joins, rightJoin)
-	return builder
+	query.joins = append(query.joins, rightJoin)
+	return query
 }
 
-func (builder *QueryBuilder) InnerJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
-	innerJoin := builder.buildJoin(tableName, firstColumn, secondColumn)
+func (query *QueryBuilder) InnerJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
+	innerJoin := query.buildJoin(tableName, firstColumn, secondColumn)
 	innerJoin.JoinType = elements.JoinInner
-	builder.joins = append(builder.joins, innerJoin)
-	return builder
+	query.joins = append(query.joins, innerJoin)
+	return query
 }
 
-func (builder *QueryBuilder) OuterJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
-	outerJoin := builder.buildJoin(tableName, firstColumn, secondColumn)
+func (query *QueryBuilder) OuterJoin(tableName elements.TableName, firstColumn string, secondColumn string) *QueryBuilder {
+	outerJoin := query.buildJoin(tableName, firstColumn, secondColumn)
 	outerJoin.JoinType = elements.JoinOuter
-	builder.joins = append(builder.joins, outerJoin)
-	return builder
+	query.joins = append(query.joins, outerJoin)
+	return query
 }
 
-func (builder QueryBuilder) Update(values map[string]string) string {
+func (query QueryBuilder) Update(values map[string]string) string {
 	sqlCommand := strings.Builder{}
 	sqlCommand.WriteString("UPDATE ")
-	sqlCommand.WriteString(builder.tableName.String())
+	sqlCommand.WriteString(query.tableName.String())
 	sqlCommand.WriteString(" SET ")
 	counter := 0
 	for column, field := range values {
@@ -85,20 +85,20 @@ func (builder QueryBuilder) Update(values map[string]string) string {
 		sqlCommand.WriteString(field)
 		sqlCommand.WriteRune('\'')
 	}
-	if builder.where != nil {
+	if query.where != nil {
 		sqlCommand.WriteString(" WHERE ")
-		sqlCommand.WriteString(builder.where.Build())
+		sqlCommand.WriteString(query.where.Build())
 	}
 	return sqlCommand.String()
 }
 
-func (builder QueryBuilder) Insert(values ...string) string {
+func (query QueryBuilder) Insert(values ...string) string {
 	sqlCommand := strings.Builder{}
 	sqlCommand.WriteString("INSERT INTO ")
-	sqlCommand.WriteString(builder.tableName.Name)
+	sqlCommand.WriteString(query.tableName.Name)
 	sqlCommand.WriteRune('(')
 
-	for index, fieldName := range builder.fields {
+	for index, fieldName := range query.fields {
 		if index > 0 {
 			sqlCommand.WriteRune(',')
 		}
@@ -116,13 +116,13 @@ func (builder QueryBuilder) Insert(values ...string) string {
 	return sqlCommand.String()
 }
 
-func (builder QueryBuilder) Select() string {
+func (query QueryBuilder) Select() string {
 	sqlCommand := strings.Builder{}
 	sqlCommand.WriteString("SELECT ")
-	if len(builder.fields) < 0 {
+	if len(query.fields) < 0 {
 		sqlCommand.WriteRune('*')
 	} else {
-		for index, fieldName := range builder.fields {
+		for index, fieldName := range query.fields {
 			if index > 0 {
 				sqlCommand.WriteRune(',')
 			}
@@ -130,13 +130,13 @@ func (builder QueryBuilder) Select() string {
 		}
 	}
 	sqlCommand.WriteString(" FROM ")
-	sqlCommand.WriteString(builder.tableName.Name)
-	if builder.where != nil {
+	sqlCommand.WriteString(query.tableName.Name)
+	if query.where != nil {
 		sqlCommand.WriteString(" WHERE ")
-		sqlCommand.WriteString(builder.where.Build())
+		sqlCommand.WriteString(query.where.Build())
 	}
-	if len(builder.organizers) > 0 {
-		for _, item := range builder.organizers {
+	if len(query.organizers) > 0 {
+		for _, item := range query.organizers {
 			sqlCommand.WriteRune(' ')
 			sqlCommand.WriteString(item.Build())
 		}
@@ -145,13 +145,13 @@ func (builder QueryBuilder) Select() string {
 	return sqlCommand.String()
 }
 
-func (builder QueryBuilder) Delete() string {
+func (query QueryBuilder) Delete() string {
 	sqlCommand := strings.Builder{}
 	sqlCommand.WriteString("DELETE FROM ")
-	sqlCommand.WriteString(builder.tableName.Name)
-	if builder.where != nil {
+	sqlCommand.WriteString(query.tableName.Name)
+	if query.where != nil {
 		sqlCommand.WriteString(" WHERE ")
-		sqlCommand.WriteString(builder.where.Build())
+		sqlCommand.WriteString(query.where.Build())
 	}
 	return sqlCommand.String()
 }
