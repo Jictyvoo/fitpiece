56b40927258dccf36eea333262db9cd47b6945ac
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-03-26 03:52:00 -0300

    :skier: test: [WIP] Add tests for fields in query builder

--- cimenteiro/builder/builder_test.go
+++ cimenteiro/builder/builder_test.go
@@ -1,1 +1,62 @@
 package builder
+
+import (
+	"github.com/Wrapped-Owls/fitpiece/cimenteiro/internal/elements"
+	"github.com/wrapped-owls/fitpiece/heartcore/failproof"
+	"testing"
+)
+
+func Test_QueryFields(t *testing.T) {
+	tableObj := elements.TableName{Name: "Test"}
+	query := New(tableObj)
+
+	testFields := [...]elements.FieldExpression{
+		{Name: tableObj.Column("avocado")},
+		{Name: "lemon"},
+		{Name: "juice"},
+		{Name: elements.TableName{Name: "banana"}.Column("fruit")},
+	}
+
+	query.Fields(testFields[0].Name, testFields[1].Name, testFields[2].Name, testFields[3].Name)
+	failproof.AssertEqualSlice(t, query.fields, testFields[:])
+}
+
+func Test_QueryFieldsAs(t *testing.T) {
+	tableOne := elements.TableName{Name: "table_1"}
+	query := New(tableOne)
+
+	testFields := [...]elements.FieldExpression{
+		{Name: tableOne.Column("panda"), Alias: "bear"},
+		{Name: "count(*)", Alias: "total"},
+		{Name: elements.TableName{Name: "table_2"}.Column("name"), Alias: "cats"},
+	}
+
+	query.FieldsAs(
+		testFields[0].Name, testFields[0].Alias,
+		testFields[1].Name, testFields[1].Alias,
+		testFields[2].Name, testFields[2].Alias,
+	)
+	failproof.AssertEqualSlice(t, query.fields, testFields[:])
+}
+
+func Test_QueryFieldsAs_OddParameters(t *testing.T) {
+	query := New(elements.TableName{Name: "table_0"})
+
+	testFields := [...]elements.FieldExpression{
+		{Name: elements.TableName{Name: "table_1"}.Column("panda"), Alias: "bear"},
+		{Name: "count(*)", Alias: "total"},
+		{Name: elements.TableName{Name: "table_2"}.Column("name"), Alias: "cats"},
+	}
+
+	query.FieldsAs(
+		testFields[0].Name, testFields[0].Alias,
+		testFields[1].Name, testFields[1].Alias,
+		testFields[2].Name,
+	)
+	failproof.AssertEqualSlice(
+		t, query.fields,
+		[]elements.FieldExpression{
+			testFields[0], testFields[1],
+		},
+	)
+}
--- cimenteiro/go.mod
+++ cimenteiro/go.mod
@@ -1,3 +1,7 @@
 module github.com/Wrapped-Owls/fitpiece/cimenteiro
 
 go 1.18
+
+replace github.com/wrapped-owls/fitpiece/heartcore => ../heartcore
+
+require github.com/wrapped-owls/fitpiece/heartcore v0.0.0-00010101000000-000000000000
