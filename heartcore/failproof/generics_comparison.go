fda7a2e0ccd433706a81a72a5ff32ed6972ffd36
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-03-28 03:02:52 -0300

    :champagne: test: Add boolean return to compare methods

--- heartcore/failproof/generics_comparison.go
+++ heartcore/failproof/generics_comparison.go
@@ -5,26 +5,40 @@ import (
 	"testing"
 )
 
-func AssertEqual[T comparable](t *testing.T, received, expected T) {
+func AssertEqual[T comparable](t *testing.T, received, expected T) bool {
 	if received != expected {
 		t.Errorf("Error! Fail in assertion:\nReceived: `%v`\nExpected: `%v`", received, expected)
+		return false
 	}
+	return true
 }
 
-func AssertEqualSlice[T comparable](t *testing.T, received, expected []T) {
+func AssertEqualCompare[T any](t *testing.T, compareCallback func(a, b T) int, received, expected T) bool {
+	if compareCallback(received, expected) != 0 {
+		t.Errorf("Error! Fail in assertion:\nReceived: `%v`\nExpected: `%v`", received, expected)
+		return false
+	}
+	return true
+}
+
+func AssertEqualSlice[T comparable](t *testing.T, received, expected []T) bool {
 	if len(received) != len(expected) {
 		t.Errorf("Size of slices are not equal %d - %d", len(received), len(expected))
-		return
+		return false
 	}
+	valid := true
 	for index, _ := range received {
-		AssertEqual(t, received[index], expected[index])
+		valid = valid && AssertEqual(t, received[index], expected[index])
 	}
+	return valid
 }
 
-func NoError(t *testing.T, err error) {
+func NoError(t *testing.T, err error) bool {
 	if err != nil {
 		t.Error(generateError(err.Error(), "nil Error"))
+		return false
 	}
+	return true
 }
 
 func AssertError(t *testing.T, received error, expected error) {
